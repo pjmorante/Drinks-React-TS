cca1104243ab70620e3f79770f0bd063a3de6408
Pedro Morante <pedro.morante@outlook.com>
2024-10-30 10:47:50 -0500

    add favoriteSlice

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -1,9 +1,11 @@
 import { create } from "zustand";
 import { createRecipesSlice, RecipesSliceType } from "./recipeSlice";
 import { devtools } from "zustand/middleware";
+import { createFavoritesSlice, FavoritesSliceType } from "./favoritesSlice";
 
-export const useAppStore = create<RecipesSliceType>()(
+export const useAppStore = create<RecipesSliceType & FavoritesSliceType>()(
   devtools((...a) => ({
     ...createRecipesSlice(...a),
+    ...createFavoritesSlice(...a),
   }))
 );
