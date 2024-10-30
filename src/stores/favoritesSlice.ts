1f2b55270876eb7d7847ab1e7629f9b272cc5ab5
Pedro Morante <pedro.morante@outlook.com>
2024-10-30 11:31:38 -0500

    add FavoritesSliceType

--- src/stores/favoritesSlice.ts
+++ src/stores/favoritesSlice.ts
@@ -1,10 +1,35 @@
 import { StateCreator } from 'zustand';
 import { Recipe } from '../types';
+import { createRecipesSlice, RecipesSliceType } from './recipeSlice';
 
 export type FavoritesSliceType = {
-  favorites: Recipe[]
-}
+  favorites: Recipe[];
+  handleClickFavorite: (recipe: Recipe) => void;
+  favoriteExists: (id: Recipe['idDrink']) => boolean;
+};
 
-export const createFavoritesSlice: StateCreator<FavoritesSliceType> = () => ({
-  favorites: []
-})
+export const createFavoritesSlice: StateCreator<
+  FavoritesSliceType & RecipesSliceType,
+  [],
+  [],
+  FavoritesSliceType
+> = (set, get, api) => ({
+  favorites: [],
+  handleClickFavorite: (recipe) => {
+    if (get().favoriteExists(recipe.idDrink)) {
+      set((state) => ({
+        favorites: state.favorites.filter(
+          (favorite) => favorite.idDrink !== recipe.idDrink
+        ),
+      }));
+    } else {
+      set((state) => ({
+        favorites: [...state.favorites, recipe],
+      }));
+    }
+    createRecipesSlice(set, get, api).closeModal();
+  },
+  favoriteExists: (id) => {
+    return get().favorites.some((favorite) => favorite.idDrink === id);
+  },
+});
